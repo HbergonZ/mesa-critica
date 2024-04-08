888169120e3450abd18fd1f02c8b2ee5a79af0dc
Hbergon <luishenriquebergsouza@outlook.com>
2024-04-08 16:50:08 -0400

    Iniciando repositorio

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mesa Crítica</title>
+    <link rel="show icon" href=""/>
+
+    <!-- CSS do Projeto -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+
+<body>
+    
+</body>
+
+
+</html>
